6404ebd6b59dc0a4f1a1f542a39927ec09386ee4
mmarabella <mmarabella@utexas.edu>
2020-08-18 17:29:48 -0500

    fixes mace semisupervise script

--- MACE/RUN_SEMISUPERVISED.sh
+++ MACE/RUN_SEMISUPERVISED.sh
@@ -2,9 +2,9 @@
 TASK=$1
 TASK_TYPE=$2
 
-LEVEL=0.0
+SUPERVISION=0.0
 FOLD=1
-PREFIX=$TASK\_$LEVEL\_fold$FOLD
+PREFIX=$TASK\_$SUPERVISION\_fold$FOLD
 java -jar MACE_external_lib/MACE.jar \
     --prefix $PREFIX \
     ../data/MACE/answer\_$TASK.csv 
@@ -13,52 +13,58 @@ mv *.competence ./results
 python3 evaluate.py $TASK $TASK_TYPE results/$PREFIX.prediction ../data/MACE/truth_$TASK.csv
 
 
-for LEVEL in 0.1 0.9
+for SUPERVISION in 0.1 0.9
 do
   for FOLD in {1..10}
   do
-    PREFIX=$TASK\_$LEVEL\_fold$FOLD
-    echo $LEVEL
+    PREFIX=$TASK\_$SUPERVISION\_fold$FOLD
+    echo $SUPERVISION
     java -jar MACE_external_lib/MACE.jar \
         --prefix $PREFIX \
         --controls ../data/kfolds/MACE/gold\_$PREFIX.csv \
         ../data/MACE/answer_$TASK.csv 
         mv *.prediction ./results
         mv *.competence ./results
-    python3 evaluate.py $TASK $TASK_TYPE results/$PREFIX.prediction ../data/kfolds/MACE/eval\_$PREFIX.csv --semi $LEVEL --fold $FOLD
+    python3 evaluate.py $TASK $TASK_TYPE results/$PREFIX.prediction ../data/kfolds/MACE/eval\_$PREFIX.csv \
+      --semi $SUPERVISION \
+      --fold $FOLD
   done
 done
 
 
-# for LEVEL in 0.2 0.8
-# do
-#   for FOLD in {1..5}
-#   do
-  # PREFIX=$TASK\_$LEVEL\_fold$FOLD
-#     echo $LEVEL
-#     java -jar MACE_external_lib/MACE.jar \
-#         --prefix $PREFIX \
-#         --controls ../data/kfolds/MACE/gold\_$PREFIX.csv \
-#         ../data/MACE/answer_$TASK.csv 
-#         mv *.prediction ./results
-#         mv *.competence ./results
-#     python3 evaluate.py $TASK $TASK_TYPE results/$PREFIX.prediction ../data/kfolds/MACE/eval\_$PREFIX.csv --semi $LEVEL --fold $FOLD
-#   done
-# done
+for SUPERVISION in 0.2 0.8
+do
+  for FOLD in {1..5}
+  do
+  PREFIX=$TASK\_$SUPERVISION\_fold$FOLD
+    echo $SUPERVISION
+    java -jar MACE_external_lib/MACE.jar \
+        --prefix $PREFIX \
+        --controls ../data/kfolds/MACE/gold\_$PREFIX.csv \
+        ../data/MACE/answer_$TASK.csv 
+        mv *.prediction ./results
+        mv *.competence ./results
+    python3 evaluate.py $TASK $TASK_TYPE results/$PREFIX.prediction ../data/kfolds/MACE/eval\_$PREFIX.csv \
+      --semi $SUPERVISION \
+      --fold $FOLD
+  done
+done
 
 
-# for LEVEL in 0.5
-# do
-#   for FOLD in {1..2}
-#   do
-  # PREFIX=$TASK\_$LEVEL\_fold$FOLD
-#     echo $LEVEL
-#     java -jar MACE_external_lib/MACE.jar \
-#         --prefix $PREFIX \
-#         --controls ../data/kfolds/MACE/gold\_$PREFIX.csv \
-#         ../data/MACE/answer_$TASK.csv 
-#         mv *.prediction ./results
-#         mv *.competence ./results
-#     python3 evaluate.py $TASK $TASK_TYPE results/$PREFIX.prediction ../data/kfolds/MACE/eval\_$PREFIX.csv --semi $LEVEL --fold $FOLD
-#   done
-# done
+for SUPERVISION in 0.5
+do
+  for FOLD in {1..2}
+  do
+  PREFIX=$TASK\_$SUPERVISION\_fold$FOLD
+    echo $SUPERVISION
+    java -jar MACE_external_lib/MACE.jar \
+        --prefix $PREFIX \
+        --controls ../data/kfolds/MACE/gold\_$PREFIX.csv \
+        ../data/MACE/answer_$TASK.csv 
+        mv *.prediction ./results
+        mv *.competence ./results
+    python3 evaluate.py $TASK $TASK_TYPE results/$PREFIX.prediction ../data/kfolds/MACE/eval\_$PREFIX.csv \
+      --semi $SUPERVISION \
+      --fold $FOLD
+  done
+done

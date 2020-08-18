2cdb0fd43d36f527fbad5894fda361275b5c40f9
mmarabella <mmarabella@utexas.edu>
2020-08-18 16:24:07 -0500

    renames run_semisupervised

--- MACE/RUN_SEMISUPERVISED.sh
+++ MACE/RUN_SEMISUPERVISED.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 TASK=$1
 
-java -jar MACE.jar \
+java -jar MACE_external_lib/MACE.jar \
     --prefix $TASK\_0.0\_fold1 \
     ../data/MACE/answer\_$TASK.csv 
 mv *.prediction ./results
-python3 results/evaluate_semisupervised.py $TASK categorical 0.0 1
+mv *.competence ./results
+python3 evaluate_semisupervised.py $TASK categorical 0.0 1
 
 
 for LEVEL in 0.1 0.9
@@ -13,12 +14,13 @@ do
   for FOLD in {1..10}
   do
     echo $LEVEL
-    java -jar MACE.jar \
+    java -jar MACE_external_lib/MACE.jar \
         --prefix $TASK\_$LEVEL\_fold$FOLD \
         --controls ../data/kfolds/MACE/gold\_$TASK\_$LEVEL\_fold$FOLD.csv \
         ../data/MACE/answer_$TASK.csv 
         mv *.prediction ./results
-    python3 results/evaluate_semisupervised.py $TASK categorical $LEVEL $FOLD
+        mv *.competence ./results
+    python3 evaluate_semisupervised.py $TASK categorical $LEVEL $FOLD
   done
 done
 
@@ -28,12 +30,13 @@ do
   for FOLD in {1..5}
   do
     echo $LEVEL
-    java -jar MACE.jar \
+    java -jar MACE_external_lib/MACE.jar \
         --prefix $TASK\_$LEVEL\_fold$FOLD \
         --controls ../data/kfolds/MACE/gold\_$TASK\_$LEVEL\_fold$FOLD.csv \
         ../data/MACE/answer_$TASK.csv 
         mv *.prediction ./results
-    python3 results/evaluate_semisupervised.py $TASK categorical $LEVEL $FOLD
+        mv *.competence ./results
+    python3 evaluate_semisupervised.py $TASK categorical $LEVEL $FOLD
   done
 done
 
@@ -43,15 +46,12 @@ do
   for FOLD in {1..2}
   do
     echo $LEVEL
-    java -jar MACE.jar \
+    java -jar MACE_external_lib/MACE.jar \
         --prefix $TASK\_$LEVEL\_fold$FOLD \
         --controls ../data/kfolds/MACE/gold\_$TASK\_$LEVEL\_fold$FOLD.csv \
         ../data/MACE/answer_$TASK.csv 
         mv *.prediction ./results
-    python3 results/evaluate_semisupervised.py $TASK categorical $LEVEL $FOLD
+        mv *.competence ./results
+    python3 evaluate_semisupervised.py $TASK categorical $LEVEL $FOLD
   done
 done
-
-
-rm *.competence
-mv results/*.prediction results/predictions

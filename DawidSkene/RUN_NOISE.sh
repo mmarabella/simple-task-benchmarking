861f3b5100ac9b77671d5b83d7f7f2250c28297b
mmarabella <mmarabella@utexas.edu>
2020-08-23 21:26:47 -0500

    fixes path in ds run_noise

--- DawidSkene/RUN_NOISE.sh
+++ DawidSkene/RUN_NOISE.sh
@@ -4,19 +4,19 @@
 
 TASK=$1
 
-# for NOISE in 0.35 0.45 0.55 0.65 0.75 0.85 0.95
-for NOISE in 0.35
+for NOISE in 0.35 0.45 0.55 0.65 0.75 0.85 0.95
+# for NOISE in 0.35
 do
-  # for SUPERVISION in 0.2 0.8
-  for SUPERVISION in 0.2
+  for SUPERVISION in 0.2 0.8
+  # for SUPERVISION in 0.2
   do
     
-    # for FOLD in {1..5}
-    for FOLD in 1
+    for FOLD in {1..5}
+    # for FOLD in 1
     do
       echo $NOISE
       echo $SUPERVISION
-      bin/get-another-label.sh \
+      get-another-label/bin/get-another-label.sh \
         --categories ../data/categories/categories\_$TASK.txt \
         --input ../data/noise/DS/answer\_$TASK\_noise$NOISE\_tab.csv \
         --eval ../data/kfolds/DS/eval_$TASK\_$SUPERVISION\_fold$FOLD\_tab.csv \
